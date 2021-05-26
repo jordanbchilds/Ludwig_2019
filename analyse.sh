43e8df1db748c2f99cf6e481f2b246688e7d83ee
Thomas Fulton (PGT) <c0078068@login02.cluster>
2021-05-26 12:14:57 +0100

    update analyse.sh: use bowtie2 to build alignment indices

--- analyse.sh
+++ analyse.sh
@@ -10,11 +10,10 @@
     ## load modules
 module load Python/3.8.6-GCCcore-10.2.0;
 module load SAMtools/1.12-GCC-10.2.0; 
-module load HISAT2/2.1.0-foss-2017b
-module load Bowtie2/2.3.4.2-foss-2018b
+module load HISAT2/2.1.0-foss-2017b;
+module load Bowtie2/2.3.4.2-foss-2018b;
 
 #Export sratools bin to shell PATH variable
-
 export PATH=$PATH:`pwd`/sratoolkit.2.11.0-ubuntu64/bin/;
 
 gse='GSE115218';
@@ -25,7 +24,7 @@ readarray -t rts < SRR_Acc_List.txt;
 
     ## Convert prefetched .sra files to fasta format ##
 
-# loop to convert each prefetched .sra file to fasta format
+## loop to convert each prefetched .sra file to fasta format
 #for i in "${rts[@]}"
 #do
 ##echo "next"
@@ -37,20 +36,24 @@ readarray -t rts < SRR_Acc_List.txt;
 
 
 
-## Split into nuclear sequences and mitochondrial sequences
+# Split into nuclear sequences and mitochondrial sequences
 python3 split_genome.py GCA_000001405.28_GRCh38.p13_genomic.fna;
 
 # Build indices for reference sequences
 # The second line takes many hours to complete...
-#bowtie2-build mito/mito.fna mito/mito&
-#bowtie2-build nuc/nuc.fna nuc/nuc;
+bowtie2-build --threads 8 mito/mito.fna mito/mito&
+bowtie2-build --threads 8 nuc/nuc.fna nuc/nuc;
 
 
-hisat2-build -p 8 mito/mito.fna mito/mito;
-echo histat2-build mitochondrial indices building stopped
-hisat2-build -p 8 nuc/nuc.fna nuc/nuc;
-echo histat2-build mitochondrial indices building stopped
+#hisat2-build -p 8 mito/mito.fna mito/mito;
+#echo histat2-build mitochondrial indices building stopped
+#hisat2-build -p 8 nuc/nuc.fna nuc/nuc;
+#echo histat2-build mitochondrial indices building stopped
  
+# remove .fna files so bowtie2 recognises indexes properly when aligning
+#rm mito/mito.fna;
+#rm nuc/nuc.fna;
+
 
 for rt in "${rts[@]}"
 do
@@ -64,8 +67,8 @@ do
     echo "Aligning ${rt} to mitochondrial genome...";
     #bowtie2 -p 22 -D20 -R 10 -N 1 -L 20 -i C,1 -x mito/mito -U fastq/${rt}.fastq -S ${rt}_aligned_mito.sam
     #bowtie2 -p 22 --very-sensitive-local -x mito/mito -U fastq/${rt}.fastq -S ${rt}_aligned_mito.sam;
-	bowtie2 -p 8 --very-sensitive -x ./nuc/nuc -U fastq/${rt}.fastq --un ./fastq/${rt}_unmapped.fastq -S fastq/${rt}_tmp.sam;
-	bowtie2 -p 8 --very-sensitive -x ./mito/mito -U ./fastq/${rt}_unmapped.fastq -S ${rt}_aligned_mito.sam;
+	bowtie2 -p 8 --very-sensitive -x /nobackup/proj/clsclmr/Ludwig_2019/nuc/nuc -U /nobackup/proj/clsclmr/Ludwig_2019/fastq/${rt}.fastq --un /nobackup/proj/clsclmr/Ludwig_2019/fastq/${rt}_unmapped.fastq -S /nobackup/proj/clsclmr/Ludwig_2019/fastq/${rt}_tmp.sam;
+	bowtie2 -p 8 --very-sensitive -x /nobackup/proj/clsclmr/Ludwig_2019/mito/mito -U /nobackup/proj/clsclmr/Ludwig_2019/fastq/${rt}_unmapped.fastq -S ${rt}_aligned_mito.sam;
 
     echo "Generating output files...";
     samtools view -Sb ${rt}_aligned_mito.sam -u| samtools view -h -f 0 -q 1 - >  ${rt}_unsorted.sam;
