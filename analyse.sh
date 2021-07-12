91e8d672c74e753cbb5240fe01c66e33764961d0
Thomas Fulton (PGT) <c0078068@login02.cluster>
2021-07-12 13:50:44 +0100

    update alignment commands, add qualimap instructions

--- analyse.sh
+++ analyse.sh
@@ -11,19 +11,17 @@
 module load Python/3.8.6-GCCcore-10.2.0;
 module load SAMtools/1.12-GCC-10.2.0; 
 module load HISAT2/2.1.0-foss-2017b;
-#module load Bowtie2/2.3.4.2-foss-2018b;
 module load parallel/20200522-GCCcore-10.2.0
 
 echo "modules loaded";
 
-#Export sratools bin to shell PATH variable
+# Export sratools bin to shell PATH variable
 export PATH=$PATH:`pwd`/sratoolkit.2.11.0-ubuntu64/bin/;
 
 gse='GSE115218';
 
-#readarray -t rts < ExamplePath_sra.txt;
+# read bulk ATAC-seq from TF1 cells into array
 readarray -t rts < multiQC/group_SRP149534_SRRs.txt;
-#readarray -t rts < multiQC/paired_fastq_unsplit/group_SRP149536_SRRs.txt
 
 
 
@@ -43,7 +41,7 @@ fi
 done
 
 cat dump_list.txt | parallel --jobs 8 "fastq-dump --split-files --gzip --outdir fastq/ sra/sra/{}.sra";
- 
+rm dump_list.txt;
 
 #rm -rf sra;
 
@@ -65,46 +63,50 @@ cat dump_list.txt | parallel --jobs 8 "fastq-dump --split-files --gzip --outdir 
 
   ## Align reads ##
 
-for rt in "${rts[@]}"
-do
+#for rt in "${rts[@]}"
+#do
+#
+# echo ${rt};
+# #echo Number of reads: $(cat fastq/${rt}.fastq|wc -l)/4|bc
+#
+# if [ -f "bam/${rt}_sorted.bai" ]; then
+#    echo "${rt} already aligned";
+# else 
+#    
+#    #echo "Aligning ${rt} to nuclear genome...";
+#    #hisat2 -p 8 -x nuc/ref -1 fastq/${rt}_1.fastq.gz -2 fastq/${rt}_2.fastq.gz --un fastq/${rt}_unmapped.fastq -S sam/${rt}_aligned.sam;
+#    #echo "Aligning ${rt} to mitochondrial genome...";
+#    #hisat2 -p 8 -x mito/mito -U fastq/${rt}_unmapped.fastq -S ${rt}_aligned_mito.sam;
+#
+#    echo "Generating output files...";
+##  first filter: -u outputs uncompressed bam into pipe: -h (header), -f 0 (do not output alignments with 0 bits), -q 1 (skip alignments with MAPQ quality <1)    
+#    #samtools view -@ 8 -Sb sam/${rt}_aligned.sam -u| samtools view -@ 8 -h -f 1 -q 10 > sam/${rt}_unsorted.sam; 
+#    #samtools view -@ 8 -Sb sam/${rt}_unsorted.sam -u| samtools sort --threads 8 > bam/${rt}_sorted.bam;  # -u pipes bam of rt_unsorted.sam: sorts by reference index
+#    #samtools view -h bam/${rt}_sorted.bam > ${rt}_header.sam  # why
+#    samtools index -@ 8 bam/${rt}_sorted.bam bam/${rt}_sorted.bai;  # index sorted bam file
+#    rm sam/${rt}_unsorted.sam;
+#    #rm ${rt}_header.sam;
+#    #rm bam/${rt}_sorted.bam
+#    rm sam/${rt}_aligned.sam;
+# fi
+#done
+#
+#
 
- echo ${rt};
- #echo Number of reads: $(cat fastq/${rt}.fastq|wc -l)/4|bc
-
- if [ -f "bam/${rt}_sorted_indexed.bam" ]; then
-    echo "${rt} already aligned";
- else 
-    
-    echo "Aligning ${rt} to nuclear genome...";
-    hisat2 -p 8 -x nuc/ref -1 fastq/${rt}_1.fastq.gz -2 fastq/${rt}_2.fastq.gz --un fastq/${rt}_unmapped.fastq -S ${rt}_aligned.sam;
-    #echo "Aligning ${rt} to mitochondrial genome...";
-    #hisat2 -p 8 -x mito/mito -U fastq/${rt}_unmapped.fastq -S ${rt}_aligned_mito.sam;
-
-    echo "Generating output files...";
-#  first filter: -u outputs uncompressed bam into pipe: -h (header), -f 0 (do not output alignments with 0 bits), -q 1 (skip alignments with MAPQ quality <1)    
-    samtools view -@ 8 -Sb ${rt}_aligned.sam -u| samtools view -@ 8 -h -f 0 -q 1 > ${rt}_unsorted.sam; 
-    samtools view -@ 8 -Sb ${rt}_unsorted.sam -u| samtools sort --threads 8 > bam/${rt}_sorted.bam;  # -u pipes bam of rt_unsorted.sam: sorts by reference index
-    samtools view -h bam/${rt}_sorted.bam > ${rt}_header.sam  # why
-    samtools index -@ 8 bam/${rt}_sorted.bam bam/${rt}_sorted_indexed.bam;  # index sorted bam file
-    rm ${rt}_unsorted.sam;
-    rm ${rt}_header.sam;
-    rm bam/${rt}_sorted.bam
-    rm ${rt}_aligned.sam;
- fi
-done
+#  ##Get qualimap ##
+#if ! [-f "qualimap_v2.2.1/qualimap"]; then
+#  wget https://bitbucket.org/kokonech/qualimap/downloads/qualimap_v2.2.1.zip;
+#  unzip qualimap_v2.2.1.zip;
+#  rm qualimap_v2.2.1.zip;
+#fi
+#
+#module load Java/11.0.2;
 
 
-
-  ##Get qualimap ##
-wget https://bitbucket.org/kokonech/qualimap/downloads/qualimap_v2.2.1.zip
-unzip qualimap_v2.2.1.zip
-rm qualimap_v2.2.1.zip
-
-module load Java?
-
+# ATACseqQC bioconductor package?
   ## Run qualimap ##
 # 'qualimap multiqc' needs a config file made up of two columns: sample_name path_to_bam
-qualimap_v2.2.1/qualimap multi-bamqc -d alignment_QC/qualimap_config.txt -r -outdir alignment_QC/
+#qualimap_v2.2.1/qualimap multi-bamqc -d qualimap_config.txt -r ;
 
 #  ## Get RNA-SeQC ##
 #wget http://www.broadinstitute.org/cancer/cga/tools/rnaseqc/RNA-SeQC_v1.1.8.jar
