cd45bc80d50a743c7e7dd5b59c11e9a0fab8fe9d
Thomas Fulton (PGT) <c0078068@login02.cluster>
2021-08-02 18:55:17 +0100

    update trimming commands and categories.txt

--- trim.sh
+++ trim.sh
@@ -1,12 +1,26 @@
+#!/bin/bash
+#SBATCH --workdir=/nobackup/proj/clsclmr/Ludwig_2019
+#SBATCH -p defq
+#SBATCH -A clsclmr
+#SBATCH -t 12:00:00
+#SBATCH -c 8
+#
 
-  ## Trim quality and adaptors based on experiment: edit categories.txt, eg. SRP149536 is scATAC-seq runs ##
+module load cutadapt/1.18-foss-2018b-Python-3.6.6;
 
-readarray -t types < categories.txt
-for i in types; do
+readarray -t types < categories.txt;
+
+
 # trim reads for low quality. REMEMBER: change hisat2 input to _trimmed.fastq for alignment
+for i in ${types[@]}; do
+
+## loop so SRRs are read
+
+
 echo "trimming ${i}";
-cutadapt -j 0 -q 30 -a TCGTCGGCAGCGTCAGATGTGTATAAGAGACAG -A GTCTCGTGGGCTCGGAGATGTGTATAAGAGACAG -o fastq/${i}_2_trimmed.fastq -p fastq/${i}_2_trimmed.fastq fastq/${rt}_1.fastq fastq/${rt}_2.fastq;
+cutadapt -j 0 -q 30 -a TCGTCGGCAGCGTCAGATGTGTATAAGAGACAG -A GTCTCGTGGGCTCGGAGATGTGTATAAGAGACAG -o fastq/${i}_2_trimmed.fastq.gz -p fastq/${i}_2_trimmed.fastq.gz fastq/${i}_1.fastq.gz fastq/${i}_2.fastq.gz;
 # ^ NOT reverse complement of adaptors
+
 done
 
 
