6b46399a73d6b1ba4f247fb1149f9a4c7ae96e88
Thomas-Fulton <thomas@fultondesigns.co.uk>
2021-08-23 01:19:19 +0100

    update descriptions

--- configure_sratools.sh
+++ configure_sratools.sh
@@ -1,18 +1,24 @@
+#!/bin/bash
 
 
-    ## Configure sra toolkit ##
-# (implement a check for SRA toolkit)
+    ## Download and configure sra toolkit ##
+
+  # MANUAL CONFIGURATION NECESSARY
+  # EXECUTE THE FOLLOWING COMMANDS IN YOUR TERMINAL
+  # When `vdb-config -i` opens an interactive configuration window, follow the instructions to exit, and continue to execute the following lines:
+
+
 # Download and extract NCBI SRA-toolkit: Ubuntu Lixux 64 bit archetecture version 2.11
-#wget https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/2.11.0/sratoolkit.2.11.0-ubuntu64.tar.gz;
-#tar -xzvf sratoolkit.2.11.0-ubuntu64.tar.gz;
-#rm sratoolkit.2.11.0-ubuntu64.tar.gz;
+wget https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/2.11.0/sratoolkit.2.11.0-ubuntu64.tar.gz;
+tar -xzvf sratoolkit.2.11.0-ubuntu64.tar.gz;
+rm sratoolkit.2.11.0-ubuntu64.tar.gz;
 
 # Export to shell PATH variable
 export PATH=$PATH:`pwd`/sratoolkit.2.11.0-ubuntu64/bin/;
 
  ## configure (interactive) 
-# opens, sets default settings and quits
-echo fyoxyo | vdb-config -i;
+# type "fyoxyo" when interactive display opens
+ vdb-config -i;
 
 # Update the prefetch download directory by editing SRA configuration file
 
@@ -20,6 +26,3 @@ echo '/repository/user/main/public/rt = '"\"$(pwd)/sra\"" >> $HOME/.ncbi/user-se
 echo '/repository/user/main/public/root = '"\"$(pwd)/sra\"" >> $HOME/.ncbi/user-settings.mkfg;
 
 
-#cd sra
-#vdb-config --prefetch-to-cwd;
-#cd ..
