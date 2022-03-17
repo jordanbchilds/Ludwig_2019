8877025951cabe9347f41eedd15623d7edf74cd6
Thomas-Fulton <thomas@fultondesigns.co.uk>
2022-03-17 14:45:36 +0000

    alignment and duplicate pipe, duplicate stats

--- analyse.sh
+++ analyse.sh
@@ -7,7 +7,7 @@
 #SBATCH -c 8
 #
 
-    ## load modules
+  ## load modules
 module load Python/3.8.6-GCCcore-10.2.0;
 module load SAMtools/1.12-GCC-10.2.0; 
 module load Bowtie2/2.3.4.2-foss-2018b;
@@ -23,9 +23,10 @@ readarray -t rts < group_SRP149534_SRRs.txt;
 
   ## build indices ##
 if test -f "nuc/btref.1.bt2"; then
-  echo "bowtie2-build reference indices";
+ echo "bowtie2-build reference indices already built";
 else
-  bowtie2-build --threads 8 nuc/hg38.fa nuc/btref;
+ echo "building reference indices..."
+ bowtie2-build --threads 8 nuc/hg38.fa nuc/btref;
 fi
 
 #locale;
@@ -43,47 +44,44 @@ do
  #echo Number of reads: $(cat fastq/${rt}.fastq|wc -l)/4|bc
 
  if [ -f "bam/${rt}_sorted.bam.bai" ]; then
-    echo "${rt} already aligned";
+  echo "${rt} already aligned";
  else 
    
-    echo "Aligning ${rt} to genome...";
+  echo "Aligning ${rt} to genome...";
 
-# bowtie2 parameters: -p 8 cores, -1 forward and -2 reverse read, -x ref, --local alignment (soft-clipping allowed), very sensitive (-L 20: 20 bp substrings in multiseed, -i s,1,0.50: shorter intervals between seed substrings, -D 20 -R 3: see manual), -t: time to align in stout,  out? -X 2000???.
+# bowtie2 parameters: 
+# -p 8 cores, -1 forward and -2 reverse read, -x ref, --local alignment (soft-clipping allowed), very sensitive (-L 20: 20 bp substrings in multiseed, -i s,1,0.50: shorter intervals between seed substrings, -D 20 -R 3: see manual), -t: time to align in stout,  out? -X 2000???.
 
-# samtools view parameters: - (input from stdin), -h (header), eg. -F 0 (do not output alignments with FLAG integer), eg. -q 10 (skip alignments with MAPQ quality <10), --un-gz unmapped reads to zipped file, -u outputs uncompressed bam into pipe.
+# samtools view parameters: 
+# - - (input from stdin, AGAIN to output to stout), -h (header), eg. -F 0 (do not output alignments with FLAG integer), eg. -q 10 (skip alignments with MAPQ quality <10), --un-gz unmapped reads to zipped file, -u outputs uncompressed bam into pipe.
+
+# samtools sort: normally sorts by leftmost coordinates (for indexing), -n sorts by QNAME (for fixmate - so mate pairs can be labelled) 
 
 # samtools fixmate adds mate score (ms) tags (used by markdup to select best reads): -m ms tags.
 
-# samtools sort: normally sorts by leftmost coordinates for indexing, -n sorts by QNAME (so mate pairs can be labelled with fixmate)
+# samtools markdup: -s print basic stats, -r will REMOVE duplicates, 
 
-# samtools markdup: -s print basic stats, -r REMOVE duplicates, 
+  bowtie2 -p 8 -1 fastq/${rt}_1.fastq.gz -2 fastq/${rt}_2.fastq.gz -x nuc/btref --local --sensitive -t --un-gz fastq/${rt}_unmapped.fastq | samtools view --threads 8 - -h -u | samtools sort --threads 8 -n - -u | samtools fixmate --threads 8 -m -u - - | samtools sort --threads 8 - -u | samtools markdup --threads 8 -s -u - - > bam/${rt}.bam
 
-bowtie2 -p 8 -1 fastq/${rt}_1.fastq.gz -2 fastq/${rt}_2.fastq.gz -x nuc/btref --local --sensitive -t --un-gz fastq/${rt}_unmapped.fastq | samtools view --threads 8 - -h -u | samtools sort --threads 8 -n - -u | samtools fixmate --threads 8 -m - -u | samtools sort --threads 8 - -u | samtools markdup --threads 8 -s - -u > bam/${rt}.bam
-
-# after first sort in pipe:
-#> bam/${rt}_sorted1.bam ;
-
-
-
-echo "collate and fixmate"
-#samtools collate --threads 8 bam/${rt}_sorted.bam -O | 
-samtools fixmate --threads 8 -m bam/${rt}_sorted1.bam bam/${rt}_fixmated.bam
-rm bam/${rt}_sorted1.bam  
-echo "sort by coordinates"
-samtools sort --threads 8 bam/${rt}_fixmated.bam -o bam/${rt}_sorted2.bam
-rm bam/${rt}_fixmated.bam 
-echo "markdup"
-samtools markdup --threads 8 -s bam/${rt}_sorted2.bam bam/${rt}.bam
+# Duplicate commands using intermediate files instead of piping: (after sorting by QNAME)
+#echo "collate and fixmate"
+##samtools collate --threads 8 bam/${rt}_sorted.bam -O | 
+#samtools fixmate --threads 8 -m bam/${rt}_sorted1.bam bam/${rt}_fixmated.bam
+#rm bam/${rt}_sorted1.bam  
+#echo "sort by coordinates"
+#samtools sort --threads 8 bam/${rt}_fixmated.bam -o bam/${rt}_sorted2.bam
+#rm bam/${rt}_fixmated.bam 
+#echo "markdup"
+#samtools markdup --threads 8 -s bam/${rt}_sorted2.bam bam/${rt}.bam
 
 # TODO add removal of optical duplicates with -d _ : what distance for Nextseq?
 
-    # index sorted bam files: -b .bai
-    echo "Index"; 
-#samtools sort --threads 8 bam/${rt}_marked.bam -o bam/{rt}.bam 
-samtools index -@ 8 bam/${rt}.bam ;
+  # index sorted bam files:
+  echo "Index"; 
+  samtools index -@ 8 bam/${rt}.bam ;
     
 # create bam containing only mitochondrial aligned reads
-#    samtools view --threads 8 -b -h bam/${rt}_sorted.bam MT > bam/${rt}_sorted_chrM.bam;
+#    samtools view --threads 8 -b -h bam/${rt}_sorted.bam chrM > bam/${rt}_sorted_chrM.bam;
  
     # get bam files headers: (-H header)
     #samtools view -H bam/${rt}_sorted.bam >> ../bam_summaries.txt 
@@ -98,17 +96,18 @@ export LC_ALL= ;
 locale;
 
 
-# Copy text from slurm outfile to alignment stats
+# Copy stats from slurm outfile (stout) to alignment stats
 cp slurm-${SLURM_JOB_ID}.out alignment_stdout.txt
-echo 'SRR Overall_alignment_rate' > alignment_summary.txt
+echo 'SRR Overall_alignment_rate Total_reads_bowtie2 Total_reads_markdup Total_duplicates Estimated_unique_lib_size' > alignment_and_duplicate_summary.txt
 
 for rt in "${rts[@]}"
 do
-
- echo ${rt};
- overall=`grep -A 20 "$rt" alignment_stdout.txt | grep "overall" | cut -d "%" -f 1`;
- echo "$rt $overall" >> alignment_summary.txt;
-
+ overall_alignment=`grep -A 20 "$rt" alignment_stdout.txt | grep "overall" | cut -d "%" -f 1`;
+ bowtie2_total_reads=`grep -A 20 "$rt" alignment_stdout.txt | grep "reads; of these:" | cut -d " " -f 1`;
+ markdup_total_reads=`grep -A 41 "$rt" alignment_stdout.txt | grep "READ:" | cut -d " " -f 2`;
+ total_duplicates=`grep -A 41 "$rt" alignment_stdout.txt | grep "DUPLICATE TOTAL:" | cut -d " " -f 3`;
+ unique_lib_size=`grep -A 41 "$rt" alignment_stdout.txt | grep "ESTIMATED_LIBRARY_SIZE" | cut -d " " -f 2`; 
+ echo "$rt $overall_alignment $bowtie2_total_reads $markdup_total_reads $total_duplicates $unique_lib_size" >> alignment_and_duplicate_summary.txt 
 done
 
 #grep chrM reference.fa | bcftools consensus calls.vcf.gz > consensus.fa
